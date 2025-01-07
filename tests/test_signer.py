dd0b8065e07895283e8a099e9ac4246934f19684
pybt <piyaboot@inox.co.th>
2025-01-07 22:01:49 +0700

    refactor(tests): reformat test cases for ByteArkSigner
    - Applied consistent multiline formatting for `sign()` function calls.
    - Improved readability by aligning arguments consistently.

--- tests/test_signer.py
+++ tests/test_signer.py
@@ -13,73 +13,84 @@ def signer() -> ByteArkSigner:
 
 
 def test_byteark_signer(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8", 1514764800)
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_expires=1514764800"
-            "&x_ark_signature=cLwtn96a-YPY7jt8ZKSf_Q"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_expires=1514764800"
+        "&x_ark_signature=cLwtn96a-YPY7jt8ZKSf_Q"
     )
 
 
 def test_byteark_signer_sign_with_HEAD_method(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {"method": "HEAD"})
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {"method": "HEAD"},
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_expires=1514764800"
-            "&x_ark_signature=QULE8DQ08f8fhFC-1gDUWQ"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_expires=1514764800"
+        "&x_ark_signature=QULE8DQ08f8fhFC-1gDUWQ"
     )
 
 
 def test_byteark_signer_sign_with_path_prefix(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {"path_prefix": "/video-objects/QDuxJm02TYqJ/"})
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {"path_prefix": "/video-objects/QDuxJm02TYqJ/"},
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_expires=1514764800"
-            "&x_ark_path_prefix=%2Fvideo-objects%2FQDuxJm02TYqJ%2F"
-            "&x_ark_signature=334wInm0jKfC6LCm23zndA"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_expires=1514764800"
+        "&x_ark_path_prefix=%2Fvideo-objects%2FQDuxJm02TYqJ%2F"
+        "&x_ark_signature=334wInm0jKfC6LCm23zndA"
     )
 
 
 def test_byteark_signer_sign_with_client_ip_dash(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {"client_ip": "103.253.132.65"})
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {"client_ip": "103.253.132.65"},
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_client_ip=1"
-            "&x_ark_expires=1514764800"
-            "&x_ark_signature=Gr9T_ZdHDy8l8CCPxpFjNg"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_client_ip=1"
+        "&x_ark_expires=1514764800"
+        "&x_ark_signature=Gr9T_ZdHDy8l8CCPxpFjNg"
     )
 
 
 def test_byteark_signer_sign_with_client_ip_underscore(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {"client-ip": "103.253.132.65"})
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {"client-ip": "103.253.132.65"},
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_client_ip=1"
-            "&x_ark_expires=1514764800"
-            "&x_ark_signature=Gr9T_ZdHDy8l8CCPxpFjNg"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_client_ip=1"
+        "&x_ark_expires=1514764800"
+        "&x_ark_signature=Gr9T_ZdHDy8l8CCPxpFjNg"
     )
 
 
@@ -89,40 +100,44 @@ def test_byteark_signer_sign_with_user_agent(signer: ByteArkSigner):
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/58.0.3029.68 Safari/537.36"
     )
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {
-                                 "client-ip": "103.253.132.65",
-                                 "user-agent": user_agent,
-                             })
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {
+            "client-ip": "103.253.132.65",
+            "user-agent": user_agent,
+        },
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_client_ip=1"
-            "&x_ark_expires=1514764800"
-            "&x_ark_signature=yYFkwZolbxCarOLHuKjD7w"
-            "&x_ark_user_agent=1"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_client_ip=1"
+        "&x_ark_expires=1514764800"
+        "&x_ark_signature=yYFkwZolbxCarOLHuKjD7w"
+        "&x_ark_user_agent=1"
     )
 
 
 def test_byteark_signer_sign_with_client_ip_with_path_prefix(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
-                             1514764800,
-                             {
-                                 "client-ip": "103.253.132.65",
-                                 "path_prefix": "/video-objects/QDuxJm02TYqJ/",
-                             })
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8",
+        1514764800,
+        {
+            "client-ip": "103.253.132.65",
+            "path_prefix": "/video-objects/QDuxJm02TYqJ/",
+        },
+    )
     assert (
-            signed_url ==
-            "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
-            "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
-            "&x_ark_auth_type=ark-v2"
-            "&x_ark_client_ip=1"
-            "&x_ark_expires=1514764800"
-            "&x_ark_path_prefix=%2Fvideo-objects%2FQDuxJm02TYqJ%2F"
-            "&x_ark_signature=2bkwVFSu6CzW7KmzXkwDbA"
+        signed_url
+        == "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+        "?x_ark_access_id=2Aj6Wkge4hi1ZYLp0DBG"
+        "&x_ark_auth_type=ark-v2"
+        "&x_ark_client_ip=1"
+        "&x_ark_expires=1514764800"
+        "&x_ark_path_prefix=%2Fvideo-objects%2FQDuxJm02TYqJ%2F"
+        "&x_ark_signature=2bkwVFSu6CzW7KmzXkwDbA"
     )
 
 
@@ -135,6 +150,8 @@ def test_byteark_signer_create_default_expire(signer: ByteArkSigner):
 
 
 def test_byteark_signer_sign_using_default_expire(signer: ByteArkSigner):
-    signed_url = signer.sign("http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8")
+    signed_url = signer.sign(
+        "http://inox.qoder.byteark.com/video-objects/QDuxJm02TYqJ/playlist.m3u8"
+    )
     expire = signer._create_default_expire()
     assert str(expire) in signed_url
