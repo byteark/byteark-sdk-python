6d3689f2f71bdd1b06633a8b542fb898dabc751f
pybt <piyaboot@inox.co.th>
2025-01-07 22:01:24 +0700

    style(signer): enforce consistent use of double quotes for strings
    - Refactored all single-quoted strings to double quotes for consistency.
    - Reformatted complex expressions for better readability.

--- byteark_sdk/signer.py
+++ byteark_sdk/signer.py
@@ -12,18 +12,18 @@ class MissingOptions(Exception):
 
 class ByteArkSigner:
     def __init__(self, **options):
-        self.access_key = options.get('access_key')
-        self.access_secret = options.get('access_secret')
-        self.default_age = options.get('default_age', 900)
-        self.skip_url_encode = options.get('skip_url_encode', False)
+        self.access_key = options.get("access_key")
+        self.access_secret = options.get("access_secret")
+        self.default_age = options.get("default_age", 900)
+        self.skip_url_encode = options.get("skip_url_encode", False)
 
         self._check_options()
 
     def _check_options(self):
         if not self.access_key:
-            raise MissingOptions('access_key is required')
+            raise MissingOptions("access_key is required")
         if not self.access_secret:
-            raise MissingOptions('access_secret is required')
+            raise MissingOptions("access_secret is required")
 
     def _make_string_to_sign(self, url: str, expire: int, options: dict = {}):
         parsed_url = urlparse(url)
@@ -52,8 +52,8 @@ class ByteArkSigner:
 
     def _make_signature(self, string_to_sign: str):
         h = hashlib.md5()
-        h.update(string_to_sign.encode('utf-8'))
-        hash_str = base64.b64encode(h.digest()).decode('utf-8')
+        h.update(string_to_sign.encode("utf-8"))
+        hash_str = base64.b64encode(h.digest()).decode("utf-8")
 
         hash_str = hash_str.replace("+", "-")
         hash_str = hash_str.replace("/", "_")
@@ -61,7 +61,9 @@ class ByteArkSigner:
         return hash_str
 
     def _create_default_expire(self) -> int:
-        return int((datetime.now(UTC) + timedelta(seconds=self.default_age)).timestamp())
+        return int(
+            (datetime.now(UTC) + timedelta(seconds=self.default_age)).timestamp()
+        )
 
     def sign(self, url: str, expire: int = 0, options: dict = {}) -> str:
         if expire == 0:
@@ -74,12 +76,19 @@ class ByteArkSigner:
             options_[k] = v
         options = options_
 
-        params = OrderedDict([
-            ("x_ark_access_id", self.access_key),
-            ("x_ark_auth_type", "ark-v2"),
-            ("x_ark_expires", expire),
-            ("x_ark_signature", self._make_signature(self._make_string_to_sign(url, expire, options))),
-        ])
+        params = OrderedDict(
+            [
+                ("x_ark_access_id", self.access_key),
+                ("x_ark_auth_type", "ark-v2"),
+                ("x_ark_expires", expire),
+                (
+                    "x_ark_signature",
+                    self._make_signature(
+                        self._make_string_to_sign(url, expire, options)
+                    ),
+                ),
+            ]
+        )
 
         if "path_prefix" in options:
             params["x_ark_path_prefix"] = options["path_prefix"]
